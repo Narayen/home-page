43d46f33fe6de8ae83103a0825e91b0e884a91ea
NaRaYeN <36738256+Narayen@users.noreply.github.com>
2021-01-25 00:01:05 +0530

    trail 21

--- public/js/index.js
+++ public/js/index.js
@@ -2,3 +2,8 @@ function insta()
 {
     window.open('https://nana-insta-downloader.netlify.app/', '_blank');
 }
+
+function clock()
+{
+    window.open('https://clock-simple.netlify.app/', '_blank');
+}
