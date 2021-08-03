34704088554626ae22b3e8e1ff1b0136dc49068e
NaRaYeN <36738256+Narayen@users.noreply.github.com>
2021-08-03 22:54:28 +0530

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -17,3 +17,68 @@ function color_code()
 {
     window.open('https://random-color-code-generator.netlify.app/', '_blank');
 }
+
+var data_js = {
+    "access_token": "5u9c9i5gb28ndri4t36yfto9"
+};
+
+function onload_sendEmail()
+{
+    sendButton.value='Sending…';
+    sendButton.disabled=true;
+
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function()
+    {
+        if (request.readyState == 4 && request.status == 200)
+        {
+            js_onSuccess();
+        } 
+        else
+        {
+            if(request.readyState == 4)
+            {
+                js_onError(request.response);
+            }
+            else
+            {
+                console.log('error');
+            }
+        }
+    };
+
+    var subject = 'Home Page Opened Alert';
+    var message = 'Someone has opened the home page without your concent';
+
+    data_js['subject'] = subject;
+    data_js['text'] = message;
+
+    var params = toParams(data_js);
+
+    request.open("POST", "https://postmail.invotes.com/send", true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    request.send(params);
+
+    return false;
+}
+
+function js_onSuccess() {
+    // remove this to avoid redirect
+    // window.location = window.location.pathname + "?message=Email+Successfully+Sent%21&isError=0";
+}
+
+function js_onError(error) {
+    // remove this to avoid redirect
+    // window.location = window.location.pathname + "?message=Email+could+not+be+sent.&isError=1";
+}
+
+function toParams(data_js) {
+    var form_data = [];
+    for ( var key in data_js ) {
+        form_data.push(encodeURIComponent(key) + "=" + encodeURIComponent(data_js[key]));
+    }
+
+    return form_data.join("&");
+}
